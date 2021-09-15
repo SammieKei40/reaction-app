2d30605023170cf92a4acd1bf4d13663fd816113
Sammie Kei <shodiposamuel1@gmail.com>
2021-09-15 11:36:44 -0700

    Deploy

---
+++ src/components/Result.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction Time: {{score}}ms</p> 
+  <p class="rank"> {{rank}} &#128540; </p>
+</template>
+
+<script>
+export default {
+props: ['score'],
+data() {
+    return {
+        rank: null
+    }
+},
+mounted(){
+        if(this.score < 250){
+            this.rank = "Awesome Reflexes!!!"  
+        } else if(this.score < 400){
+            this.rank = "Rapid Reflexes!!!"
+        } else {
+            this.rank = "Snail Pace..." 
+        }
+    }
+}
+</script>
+
+<style>
+    .rank{
+        font-size: 1.4em;
+        color: #000;
+        font-weight: bold;
+    }
+</style>
